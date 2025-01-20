f23678c09e3bb16bcb3b855b95dd2fb28d6dff38
BaitemirAsanbaev <baitemir1706@gmail.com>
2025-01-20 14:53:18 +0600

    work module

---
+++ src/work/work.ts
@@ -0,0 +1,56 @@
+import { BelongsToMany, Column, DataType, Model, Table } from 'sequelize-typescript';
+import { Stack } from 'src/stack/stack';
+import { WorkStack } from 'src/work-stack/work-stack';
+
+@Table({
+  tableName: 'work',
+  timestamps: true,
+})
+export class Work extends Model<Work> {
+  @Column({
+    type: DataType.INTEGER,
+    unique: true,
+    autoIncrement: true,
+    primaryKey: true,
+  })
+  id: number;
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  title: string;
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  site_url: string;
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  code_url: string;
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  brief: string;
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  full: string;
+  @Column({
+    type: DataType.ARRAY(DataType.STRING),
+    allowNull: false,
+  })
+  images: string[];
+  @Column({
+    type: DataType.ARRAY(DataType.STRING),
+    allowNull: false,
+  })
+  stack_info: string[];
+
+  @BelongsToMany(() => Stack, () => WorkStack)
+  stack: Stack[];
+  
+} 
